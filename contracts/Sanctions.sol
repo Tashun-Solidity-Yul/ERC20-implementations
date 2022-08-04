21991494f7312697c5cecc70ad7d0877677107ef
tashun <tashun.chamara@gmail.com>
2022-08-04 23:09:19 +0530

    sanctions add

---
+++ contracts/Sanctions.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Import this file to use console.log
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Sanctions is ERC20 {
+    constructor(string memory tokenName, string memory tokenSymbol, uint256 initialSupply) ERC20(tokenName, tokenSymbol) {
+        _mint(msg.sender, initialSupply);
+    }
+
+
+    function restrictAddress(address restrictingAddress) pure public {
+
+    }
+}
