442575b7e8af7905411797000ed52a0417e2f532
tashun <tashun.chamara@gmail.com>
2022-08-07 04:13:11 +0530

    added god mod and sanctions

--- contracts/GodMod.sol
+++ contracts/GodMod.sol
@@ -4,19 +4,34 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
-pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import {Unauthorized,InSufficientFunds,InvalidInputDetected, BaseContract} from "./utils/GeneralUtils.sol";
 
-contract GodMod is ERC20 {
-    constructor(string memory tokenName, string memory tokenSymbol, uint256 initialSupply) ERC20(tokenName, tokenSymbol) {
-        _mint(msg.sender, initialSupply);
+contract GodMod is ERC20, BaseContract {
+    
+
+    constructor(string memory tokenName, string memory tokenSymbol) ERC20(tokenName, tokenSymbol) {
+        owner= msg.sender;
     }
 
 
-    function mintTokensToAddress(address recipient) public pure {}
+    function mintTokensToAddress(address recipient, uint supply) external ownerCheck {
+        validateAddress(recipient);
+        validateInt(supply);
+        _mint(recipient, supply);
+    }
 
-    function changeBalanceAtAddress(address target) public pure {}
+    function changeBalanceAtAddress(address target, uint supply) external ownerCheck {
+        validateAddress(target);
+        validateInt(supply);
+        _burn(target, supply);
+    }
 
-    function authoritativeTransferFrom(address from, address to)  public pure{}
+    function authoritativeTransferFrom(address from, address to, uint amount)  external ownerCheck {
+        validateAddress(from);
+        validateAddress(to);
+        validateInt(amount);
+        _transfer(from, to ,amount);
+    }
 }
